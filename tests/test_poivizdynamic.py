a05fd1725dcd4e873003e27a2683b0c6eb0a8532
Tianqing <tz2372@gmail.com>
2021-12-17 16:34:09 -0500

    update

--- tests/test_poivizdynamic.py
+++ tests/test_poivizdynamic.py
@@ -1,5 +1,6 @@
 from poivizdynamic import poivizdynamic as pv
 from poivizdynamic import __version__
+import os
 import pandas as pd
 
 df = pd.read_csv("src/poivizdynamic/data/demo_fake_data.csv")
@@ -7,14 +8,16 @@ travel = pv.get_demo_data(df, "my travel map")
 
 
 def test_version():
-    assert __version__ == "0.1.4"
+    assert __version__ == "0.1.5"
 
 
 def test_get_geo_dataset_preprocess(df=travel):
-    df_us = pv.get_geo_dataset(df, maptype="us")
+    api_key_us = os.getenv("GEO_CENSUS_API_KEY")
+    df_us = pv.get_geo_dataset(api_key_us, df, maptype="us")
     assert df_us.shape[1] == 12
 
-    df_world = pv.get_geo_dataset(df, maptype="world")
+    api_key_w = os.getenv("GEO_RADAR_API_KEY")
+    df_world = pv.get_geo_dataset(api_key_w, df, maptype="world")
     assert df_world.shape[1] == 12
 
     df_world = pv.clean_dataset(df_world)
