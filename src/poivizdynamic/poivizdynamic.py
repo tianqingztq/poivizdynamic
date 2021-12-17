c117b79b3f2c37bbdaf080c5aebe3e7565b05f71
Tianqing <tz2372@gmail.com>
2021-12-17 12:08:31 -0500

    add bubble map function

--- src/poivizdynamic/poivizdynamic.py
+++ src/poivizdynamic/poivizdynamic.py
@@ -1,14 +1,15 @@
 import requests
 import pandas as pd
+from statistics import median
 import os
 # from requests_oauthlib import OAuth1Session
-import geopandas as gpd
 import time
 from datetime import datetime, timedelta
 # from dateutil import rrule
 import warnings
 
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 def get_coordinate_api(addr):
@@ -33,19 +34,6 @@ def get_coordinate_api(addr):
             end = time.time()
             print(f'Requested the api in {end - start:0.4f} seconds')
             return out_df
-
-# get_coordinate_api('20 Jay Street, Brooklyn, New York, NY')
-    
-
-
-def get_demo_data(df, demo_data = "my travel map"):
-    if demo_data == "my travel map":
-        df = df.iloc[0:4,]
-    elif demo_data == "life log":
-        df = df.iloc[4:7,]
-    elif demo_data == "starbuck":
-        df = df.iloc[7:,]
-    return df
 
 
 def clean_dataset(df):
@@ -76,10 +64,11 @@ def get_geo_dataset(df):
     return df_out
 
 
-def get_footprint_map(df_in, title = "My animated map", fig_name = 'my_animate_map', zoom = 2.5):
+def get_footprint_map(df_in, fig_name = 'my_animate_map', title_text = "My animated map", title_size = 20, zoom = 2.5):
     # https://docs.mapbox.com/help/getting-started/access-tokens/
 
     TOKEN_MAPBOX = os.getenv('TOKEN_MAPBOX')
+    
 
     lon = [df_in["longitude"][0]]
     lat = [df_in["latitude"][0]]
@@ -129,7 +118,6 @@ def get_footprint_map(df_in, title = "My animated map", fig_name = 'my_animate_m
             mapbox = {
             'accesstoken': TOKEN_MAPBOX,
             'style': "light", 'zoom': zoom},
-            title_text = title
         ),
         
         frames = frames
@@ -138,12 +126,14 @@ def get_footprint_map(df_in, title = "My animated map", fig_name = 'my_animate_m
 
     fig.update_layout(
                     mapbox_center = {"lat": mid_lat, "lon": mid_lon},
-                    margin={"r":0,"t":40,"l":0,"b":0}
+                    margin={"r":0,"t":40,"l":0,"b":0},
+                    title_text = title_text,
+                    title_font_size = title_size
                       
                      )
 
-
-    fig.show()
+    
+    # fig.show()
     
     # save the html dynamic file
     if not os.path.exists('demo_output'):
@@ -152,3 +142,84 @@ def get_footprint_map(df_in, title = "My animated map", fig_name = 'my_animate_m
     html_path = f'demo_output/{fig_name}.html'
     #html_path = f'{fig_name}.html'
     fig.write_html(html_path)
+    return fig
+
+# bubble plot
+def get_animated_bubble_map(df, title_text = "My animated bubble map with value/ colored with spot or group", title_size = 20, color_group_lab = "spot_name", color_value_discrete = True, bubble_size = "interest_value", radius:'int > 0' = 20, zoom = 2.5, fig_name = "my_animated_bubble_plot")-> None:
+
+    
+    lat_lab = "latitude"
+    lon_lab = "longitude"
+   
+    date = df["date"].apply(lambda x: x.strftime('%Y-%m-%d'))
+    
+    mid_lat = median(df[lat_lab])
+
+    mid_lon = median(df[lon_lab])
+    
+    if type(bubble_size) == "str":
+        
+        df[bubble_size] = df[bubble_size].apply(lambda x: pd.to_numeric(x))
+    else:
+        const_num = bubble_size
+        bubble_size = [const_num] * len(df)
+
+    if color_value_discrete == True:
+        
+        fig = px.scatter_mapbox(df, lat = lat_lab,
+                                lon = lon_lab,
+                                size = bubble_size,
+                                color = color_group_lab,
+                                animation_frame = date,
+                                color_discrete_sequence = px.colors.qualitative.D3,  
+                                # https://plotly.com/python/discrete-color/
+                                size_max = radius,
+                                # labels = {value_lab:'income'} # only for continuous data
+                               )
+    else:
+        fig = px.scatter_mapbox(df, lat = lat_lab,
+                                lon = lon_lab,
+                                size = bubble_size,
+                                color = color_group_lab,
+                                animation_frame = date,
+                                color_continuous_scale=px.colors.cyclical.IceFire,
+                                #color_continuous_scale=px.colors.sequential.Viridis,
+                                #color_discrete_sequence = px.colors.qualitative.D3,  
+                                # https://plotly.com/python/discrete-color/
+                                size_max = radius,
+                                # labels = {value_lab:'income'} # only for continuous data
+                               )
+    fig.update_layout(mapbox_style="carto-positron", 
+                      mapbox_zoom=zoom, 
+                      mapbox_center = {"lat": mid_lat, "lon": mid_lon},
+                      
+                     )
+    fig.update_layout(margin={"r":0,"t":40,"l":0,"b":0})
+    fig.update_layout(title_text = title_text, title_font_size = title_size)
+    
+    fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 600
+    fig.layout.updatemenus[0].buttons[0].args[1]["transition"]["duration"] = 600
+    fig.layout.coloraxis.showscale = True   
+    fig.layout.sliders[0].pad.t = 10
+    fig.layout.updatemenus[0].pad.t= 10 
+    
+    # save the html dynamic file
+    if not os.path.exists('demo_output'):
+        os.makedirs('demo_output')
+
+    html_path = f'demo_output/{fig_name}.html'
+    #html_path = f'{fig_name}.html'
+    fig.write_html(html_path)
+    
+    return fig
+
+
+
+def get_demo_data(df, demo_data = "my travel map"):
+    if demo_data == "my travel map":
+        df = df.iloc[0:4,]
+    elif demo_data == "life log":
+        df = df.iloc[4:7,]
+    elif demo_data == "starbuck":
+        df = df.iloc[7:,]
+    return df
